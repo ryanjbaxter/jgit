3f856008651692d2f7f367d1602fefbd631f7f89
Ryan Baxter <rbaxter@pivotal.io>
2016-08-15 09:21:31 -0400

    updates

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,12 +1,21 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableFeignClients
 public class DemoApplication implements CommandLineRunner{
+
+	@Autowired
+	RestClient client;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
@@ -36,7 +45,16 @@ public class DemoApplication implements CommandLineRunner{
 //		}
 
 		RestTemplate rest = new RestTemplate();
-		String result = rest.getForObject("http://example.com", String.class);
+//		String result = rest.getForObject("http://example.com", String.class);
+		String result = client.hello();
 		System.out.println(result);
+
 	}
+}
+
+@FeignClient(name = "example", url = "example.com")
+@RequestMapping("/")
+interface RestClient {
+	@RequestMapping(method = RequestMethod.GET)
+	String hello();
 }
