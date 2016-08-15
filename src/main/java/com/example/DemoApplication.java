d295f77bba43e1cfb1f2917d0b49dbfebc4747b9
Ryan Baxter <rbaxter@pivotal.io>
2016-08-15 09:13:57 -0400

    updates

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,14 +1,9 @@
 package com.example;
 
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.util.FileUtils;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner{
@@ -19,25 +14,29 @@ public class DemoApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... strings) throws Exception {
-		try {
-			final File basedir = Files.createTempDirectory("config-repo-").toFile();
-			Runtime.getRuntime().addShutdownHook(new Thread() {
-				@Override
-				public void run() {
-					try {
-						FileUtils.delete(basedir, FileUtils.RECURSIVE);
-					}
-					catch (IOException e) {
+//		try {
+//			final File basedir = Files.createTempDirectory("config-repo-").toFile();
+//			Runtime.getRuntime().addShutdownHook(new Thread() {
+//				@Override
+//				public void run() {
+//					try {
+//						FileUtils.delete(basedir, FileUtils.RECURSIVE);
+//					}
+//					catch (IOException e) {
+//
+//					}
+//				}
+//			});
+//			Git.cloneRepository().setURI("https://github.com/spring-cloud-samples/config-repo").
+//					setDirectory(basedir).call();
+//			System.out.println("Cloned repo to " + basedir);
+//		}
+//		catch (IOException e) {
+//			throw new IllegalStateException("Cannot create temp dir", e);
+//		}
 
-					}
-				}
-			});
-			Git.cloneRepository().setURI("https://github.com/spring-cloud-samples/config-repo").
-					setDirectory(basedir).call();
-			System.out.println("Cloned repo to " + basedir);
-		}
-		catch (IOException e) {
-			throw new IllegalStateException("Cannot create temp dir", e);
-		}
+		RestTemplate rest = new RestTemplate();
+		String result = rest.getForObject("http://example.com", String.class);
+		System.out.println(result);
 	}
 }
